253665d3ce50fa3e7da3c194c69866b7effc92c6
eme64 <emanuel.peter@hotmail.com>
2018-04-16 22:23:45 +0200

    ex07 q2 some code already

--- ex07/q2/main.cpp
+++ ex07/q2/main.cpp
@@ -2,42 +2,160 @@
 #include <cstdlib>
 #include <random>
 
+#include <algorithm>
+#include <cmath>
+
 #include <mkl.h>
 #include <omp.h>
 
-//#include "qr.hpp"
 
-/*
-------------- EULER MEASUREMENTS (more in file: log_out)
+#define PARAMETER_P 30
+#define LAMBDA 0.1
+void metropolis_step(
+	double* bk, double* X, int N, double* y, double T, double* bk_p, double &Ek_p,
+	std::default_random_engine &generator, std::normal_distribution<double> &gaussian,
+	std::mt19937 &gen, std::uniform_real_distribution<> &uniform
+)
+{
+	// out bk_p, E
+	
+	
+	// step 1:
+	//std::default_random_engine generator;
+        //std::normal_distribution<double> gaussian(0.0,LAMBDA);
+	for(size_t i=0; i<PARAMETER_P; i++)
+	{
+		bk_p[i] = bk[i] + gaussian(generator);
+	}
+	
+	// step 2:
+	double Ek = 0;
+	Ek_p = 0;
+	
+	for(int line = 0; line < N; line++)
+        {
+                double ek = -y[line];
+		double ek_p = -y[line];
+                for(int i = 0; i < PARAMETER_P; i++)
+                {
+                        ek_p-= X[line*PARAMETER_P + i] * bk[i];
+			ek_p-= X[line*PARAMETER_P + i] * bk_p[i];
+                }
+		Ek += ek*ek;
+		Ek_p += ek_p*ek_p;
+        }
+	
+	double h = std::min(
+			1.0,
+			std::exp(-(Ek - Ek_p)/T)
+	);
+	
+	// step 3:
+	//std::random_device rd;
+        //std::mt19937 gen(rd());
+        //std::uniform_real_distribution<> uniform(-10.0, 10.0);
+	double u = uniform(gen);
+	
+	// step 4:
+	if(u <= h)
+	{
+		// reject new suggestion
+		for(size_t i=0; i<PARAMETER_P; i++)
+		{
+			bk_p[i] = bk[i];
+		}
+		return;
+	}else{
+		// keep current suggestion
+		return;
+	}
+}
 
-------------- for K=20:
-thread : seconds -> speedup
-1 : 1.55296  -> 1
-2 : 0.886962 -> 1.75
-4 : 0.649816 -> 2.40
-8 : 0.477121 -> 3.25
-12: 0.389736 -> 4.00
-18: 0.336268 -> 4.61
-23: 0.404758 -> 3.84
 
------------ same with dgels:
-1 : 1.39577  -> 1
-2 : 0.829318 -> 1.68
-4 : 0.511491 -> 2.73
-8 : 0.388919 -> 3.59
-12: 0.296205 -> 4.71
-18: 0.258826 -> 5.39
-23: 0.331876 -> 4.20
+void mcmc_routine(
+        int k_max, double* b_best, int &k_best, double &E_best, double* X, int N, double* y, double (*T)(int),
+        std::default_random_engine &generator, std::normal_distribution<double> &gaussian,
+        std::mt19937 &gen, std::uniform_real_distribution<> &uniform
+)
+{
+	// initialize a first beta:
+	double *b_curr = (double*) std::malloc(sizeof(double)*PARAMETER_P);
+	double *b_next = (double*) std::malloc(sizeof(double)*PARAMETER_P);
+	std::random_device rd;
+        std::mt19937 gen_b(rd());
+        std::uniform_real_distribution<> dis_b(-5.0, 15.0);
 
-conclusion:
-similar behaviour. 
-dgels a bit faster and a bit better scaling.
-probably due to better use of time within parts I had to seperate.
-*/
+        for(int i=0; i<PARAMETER_P; i++)
+        {
+                b_curr[i] = dis_b(gen_b); // uniform
+        }
+
+	// do a first step to get some first error:
+	metropolis_step(
+        	b_curr, X, N, y, T(0), b_next, E_best,
+        	generator, gaussian,
+        	gen, uniform
+	);
+	
+	// swap
+	std::swap(b_next, b_curr);
+
+	// set best for now:
+	for(int i=0; i<PARAMETER_P; i++)
+        {
+                b_best[i] = b_curr[i];
+        }
+	k_best = 0;
+
+	// do k_max steps:
+	for(int k=1; k<k_max; k++)
+	{
+		double error_now = 0;
+		
+		metropolis_step(
+                	b_curr, X, N, y, T(k), b_next, error_now,
+                	generator, gaussian,
+        	        gen, uniform
+	        );
+		
+		if(error_now < E_best)
+		{
+			E_best = error_now;
+			// update best beta
+			for(int i=0; i<PARAMETER_P; i++)
+		        {
+                		b_best[i] = b_curr[i];
+        		}
+			k_best = k;
+		}
+		
+
+	}
+	
+
+	std::free(b_curr);
+	std::free(b_next);
+}
 
 
-void qr(int N, int p = 30);
 
+
+void generate_data(double *b_opt, double *X, double *y, int N);
+
+double T_invlog(int k)
+{
+	return 1.0 / std::log(k+1);
+}
+
+double T_inv(int k)
+{
+	return 1.0 / (double)(k+1);
+}
+
+double T_invexp(int k)
+{
+	return std::exp(-k);
+}
 int main(int argc, char *argv[]){
 	/*
  	 * Intel MKL research:
@@ -50,26 +168,44 @@ int main(int argc, char *argv[]){
  	 * somehow solve result for R (upper triangular matrix)
  	 */
 	
-	for(int thr = 1; thr<24; thr++)
+	int N = 1 << 10; // 15
+	std::cout << "N = " << N << std::endl;
+
+	// allocate buffers:
+	std::cout << "GENERATE BUFFERS" << std::endl;
+	double* beta_optimal = (double*) std::malloc(sizeof(double)*PARAMETER_P);
+	double* X = (double*) std::malloc(sizeof(double)* PARAMETER_P*N);
+        double* y = (double*) std::malloc(sizeof(double)* N);
+	
+	// generate data:
+	std::cout << "GENERATE DATA" << std::endl;
+	generate_data(beta_optimal, X, y, N);
+
+	// do experiments:
+	std::cout << "DO EXPERIMENTS" << std::endl;
+	
+	for(int i=1; i<6; i++)
 	{
-		std::cout << "######################################## NUM THREADS: " << thr << std::endl;
-		mkl_set_num_threads(thr);
-		for(int i=15; i<21; i++)
-		{
-			qr(i);
-		}
+		double epsilon = std::pow(10, -i);
+		std::cout << "EPSILON = " << epsilon << std::endl;
+		
+		// TODO
 	}
+
+	// free buffers:
+	std::cout << "FREE DATA" << std::endl;
+	std::free(beta_optimal);
+	std::free(X);
+	std::free(y);
 }
 
-void qr(int K, int p)
+void generate_data(double *b_opt, double *X, double *y, int N)
 {
-	int N = 1 << K; // N >= p, else problem not well posed !
-	//int p = 30;
-	
-	std::cout << "#############################  K: " << K << ", N: " << N << ", p: " << p << std::endl;
-
+	const int p = PARAMETER_P;
 	// generate beta
-	double* beta = (double*) std::malloc(sizeof(double)*p);
+	double* beta = (double*) std::malloc(sizeof(double)*PARAMETER_P);
+	double* X_copy = (double*) std::malloc(sizeof(double)* PARAMETER_P*N);
+	double* y_copy = (double*) std::malloc(sizeof(double)* N);
 	
 	std::random_device rd;
 	std::mt19937 gen(rd());
@@ -81,14 +217,6 @@ void qr(int K, int p)
 		//std::cout << beta[i] << std::endl;
 	}	
 	// create random data:
-	double* X = (double*) std::malloc(sizeof(double)* p*N);
-	double* y = (double*) std::malloc(sizeof(double)* std::max(N, p)); // must have enough space for solving later
-	
-
-	// copy data:
-	double* X_copy = (double*) std::malloc(sizeof(double)* p*N);
-        double* y_copy = (double*) std::malloc(sizeof(double)* std::max(N, p));
-	
 	std::default_random_engine generator;
 	std::normal_distribution<double> gaussian(0.0,1.0);
 
@@ -101,56 +229,13 @@ void qr(int K, int p)
 			X_copy[line*p + i] = X[line*p + i];
 			y[line]+= X[line*p + i] * beta[i];
 		}
-		//std::cout << "y: " << y[line] << std::endl;
 		y_copy[line] = y[line];
 	}	
 
-	// QR decomposition
-	double* tau = (double*) std::malloc(sizeof(double) * std::max(p, N));
 	
-	double t0 = omp_get_wtime();
-	lapack_int info = LAPACKE_dgeqrf(LAPACK_ROW_MAJOR, N, p, X, p, tau);		
-	if(info != 0)
-	{
-		std::cout << "dgeqrf error: " << info << std::endl;
-	}else{
-		//std::cout << "success dgeqrf !" << std::endl;
-	}
-	
-	// apply Q to y
-	int k = std::min(p, N);
-	info = LAPACKE_dormqr(LAPACK_ROW_MAJOR, 'L', 'T', N, 1, k, X, k, tau, y, 1);
-	if(info != 0)
-        {
-                std::cout << "dormqr error: " << info << std::endl;
-        }else{
-                //std::cout << "success dormqr !" << std::endl;
-        }
-	
-	// solve result for R
-	info = LAPACKE_dtrtrs (LAPACK_ROW_MAJOR, 'U', 'N', 'N', k, 1, X, k, y, 1);
-	if(info != 0)
-        {
-                std::cout << "dtrtrs error: " << info << std::endl;
-        }else{
-                //std::cout << "success dtrtrs !" << std::endl;
-        }
-	double t1 = omp_get_wtime();	
-	std::cout << "my time: " << t1-t0 << std::endl;
-	
-	// compare betas:
-	double error = 0;
-	for(int i=0; i<p; i++)
-	{
-		//std::cout << "[" << i << "] b: " << beta[i] << ", b_est: " << y[i] << std::endl;
-		double e = beta[i] - y[i];
-		error += e*e;
-	}
-	std::cout << "my Err: " << std::sqrt(error) << std::endl;
-
-	// compare to dgels on copy of data:
+	// do least squares with dgels:
 	double t2 = omp_get_wtime();
-	info = LAPACKE_dgels (LAPACK_ROW_MAJOR, 'N', N, p, 1, X_copy, p, y_copy, 1);
+	lapack_int info = LAPACKE_dgels (LAPACK_ROW_MAJOR, 'N', N, p, 1, X_copy, p, y_copy, 1);
 	if(info != 0)
         {
                 std::cout << "dgels error: " << info << std::endl;
@@ -159,33 +244,13 @@ void qr(int K, int p)
         }
 	double t3 = omp_get_wtime();
 	std::cout << "dgels time: " << t3-t2 << std::endl;
-	// compare betas:
-	double error_dgels = 0;
-	for(int i=0; i<p; i++)
+
+	for(int i=0; i<PARAMETER_P; i++)
 	{
-		//std::cout << "[" << i << "] b: " << beta[i] << ", b_est: " << y[i] << std::endl;
-		double e = beta[i] - y[i];
-	        error_dgels += e*e;
+		b_opt[i] = y[i];
 	}
-	std::cout << "dgels Err: " << std::sqrt(error_dgels) << std::endl;
 	
-	// compare my to dgels:
-	double difference = 0;
-	for(int i=0; i<p; i++)
-	{
-		//std::cout << "[" << i << "] my: " << y[i] << ", dgels: " << y_copy[i] << std::endl;
-		double e = y[i] - y_copy[i];
-		difference+= e*e;
-	}
-	std::cout << "my vs dgels: " << difference << std::endl;
-
-
-	// free data:
 	std::free(beta);
-	std::free(y);
-	std::free(X);
-	std::free(tau);
-
-	std::free(y_copy);
 	std::free(X_copy);
+	std::free(y_copy);
 }
