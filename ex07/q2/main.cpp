8647440360f3d4ada4ff3f3dd51157288be43c5c
eme64 <emanuel.peter@hotmail.com>
2018-04-17 10:41:50 +0200

    ex07 q2 with measurements for d)

--- ex07/q2/main.cpp
+++ ex07/q2/main.cpp
@@ -10,10 +10,109 @@
 
 
 #define PARAMETER_P 30
-#define LAMBDA 0.1
+#define LAMBDA 1.0
+
+/* COMMENT:
+ *
+ * It was important to also control the variance of q(x|y)
+ * if it does not decrease with the error (or other schedule)
+ * then we will not have fast convergence
+ * */
+
+/* MEASUREMENTS (usual print outs)
+ * 
+ 
+-------------------------------
+N = 32768
+T:   T_inv 
+GENERATE BUFFERS
+GENERATE DATA
+dgels time: 0.0477881
+DO EXPERIMENTS
+REACHED 1e+10, k: 0
+REACHED 1e+09, k: 1
+REACHED 1e+08, k: 2
+REACHED 1e+07, k: 3
+REACHED 1e+06, k: 4
+REACHED 100000, k: 5
+REACHED 10000, k: 6
+REACHED 1000, k: 193
+REACHED 100, k: 531
+REACHED 10, k: 878
+REACHED 1, k: 1278
+REACHED 0.1, k: 1672
+REACHED 0.01, k: 2229
+REACHED 0.001, k: 2478
+REACHED 0.0001, k: 2857
+REACHED 1e-05, k: 3265
+REACHED 1e-06, k: 3728
+REACHED 1e-07, k: 4111
+REACHED 1e-08, k: 4494
+TERMINATE: 4495
+FREE DATA
+
+-------------------------------
+N = 32768
+T:   T_invlog 
+GENERATE BUFFERS
+GENERATE DATA
+dgels time: 0.0279679
+DO EXPERIMENTS
+REACHED 1e+10, k: 103
+REACHED 1e+09, k: 416
+REACHED 1e+08, k: 731
+REACHED 1e+07, k: 1095
+REACHED 1e+06, k: 1328
+REACHED 100000, k: 1669
+REACHED 10000, k: 1971
+REACHED 1000, k: 2406
+REACHED 100, k: 2875
+REACHED 10, k: 3228
+REACHED 1, k: 3641
+REACHED 0.1, k: 4032
+REACHED 0.01, k: 4444
+REACHED 0.001, k: 4847
+REACHED 0.0001, k: 5165
+REACHED 1e-05, k: 5597
+REACHED 1e-06, k: 6394
+^C ---> timeout / no fast convergence.
+
+-------------------------------
+N = 32768
+T:   T_invexp 
+GENERATE BUFFERS
+GENERATE DATA
+dgels time: 0.0238519
+DO EXPERIMENTS
+REACHED 1e+10, k: 0
+REACHED 1e+09, k: 1
+REACHED 1e+08, k: 2
+REACHED 1e+07, k: 3
+REACHED 1e+06, k: 4
+REACHED 100000, k: 5
+REACHED 10000, k: 6
+REACHED 1000, k: 155
+REACHED 100, k: 475
+REACHED 10, k: 1010
+REACHED 1, k: 1444
+REACHED 0.1, k: 1751
+REACHED 0.01, k: 2231
+REACHED 0.001, k: 2499
+REACHED 0.0001, k: 2987
+REACHED 1e-05, k: 3234
+REACHED 1e-06, k: 3736
+REACHED 1e-07, k: 4226
+REACHED 1e-08, k: 4470
+TERMINATE: 4471
+FREE DATA
+
+
+
+ * */
 void metropolis_step(
 	double* bk, double* X, int N, double* y, double T, double* bk_p, double &Ek_p,
 	std::default_random_engine &generator, std::normal_distribution<double> &gaussian,
+	double lambda,
 	std::mt19937 &gen, std::uniform_real_distribution<> &uniform
 )
 {
@@ -25,7 +124,7 @@ void metropolis_step(
         //std::normal_distribution<double> gaussian(0.0,LAMBDA);
 	for(size_t i=0; i<PARAMETER_P; i++)
 	{
-		bk_p[i] = bk[i] + gaussian(generator);
+		bk_p[i] = bk[i] + gaussian(generator)*lambda;
 	}
 	
 	// step 2:
@@ -38,8 +137,8 @@ void metropolis_step(
 		double ek_p = -y[line];
                 for(int i = 0; i < PARAMETER_P; i++)
                 {
-                        ek_p-= X[line*PARAMETER_P + i] * bk[i];
-			ek_p-= X[line*PARAMETER_P + i] * bk_p[i];
+                        ek+= X[line*PARAMETER_P + i] * bk[i];
+			ek_p+= X[line*PARAMETER_P + i] * bk_p[i];
                 }
 		Ek += ek*ek;
 		Ek_p += ek_p*ek_p;
@@ -47,8 +146,9 @@ void metropolis_step(
 	
 	double h = std::min(
 			1.0,
-			std::exp(-(Ek - Ek_p)/T)
+			std::exp(-(Ek_p - Ek)/T)
 	);
+	//std::cout << "h: " << h << ", Ek: " << Ek << ", Ek_p: " << Ek_p << std::endl;
 	
 	// step 3:
 	//std::random_device rd;
@@ -57,16 +157,18 @@ void metropolis_step(
 	double u = uniform(gen);
 	
 	// step 4:
-	if(u <= h)
+	if(u > h)
 	{
 		// reject new suggestion
 		for(size_t i=0; i<PARAMETER_P; i++)
 		{
 			bk_p[i] = bk[i];
 		}
+		//std::cout << "back" << std::endl;
 		return;
 	}else{
 		// keep current suggestion
+		//std::cout << "keep" << std::endl;
 		return;
 	}
 }
@@ -94,6 +196,7 @@ void mcmc_routine(
 	metropolis_step(
         	b_curr, X, N, y, T(0), b_next, E_best,
         	generator, gaussian,
+		1.0,
         	gen, uniform
 	);
 	
@@ -115,8 +218,11 @@ void mcmc_routine(
 		metropolis_step(
                 	b_curr, X, N, y, T(k), b_next, error_now,
                 	generator, gaussian,
+			1.0,
         	        gen, uniform
 	        );
+		
+		std::swap(b_curr, b_next);
 		
 		if(error_now < E_best)
 		{
@@ -154,7 +260,7 @@ double T_inv(int k)
 
 double T_invexp(int k)
 {
-	return std::exp(-k);
+	return 1.0 * std::exp(-k);
 }
 int main(int argc, char *argv[]){
 	/*
@@ -168,9 +274,12 @@ int main(int argc, char *argv[]){
  	 * somehow solve result for R (upper triangular matrix)
  	 */
 	
-	int N = 1 << 10; // 15
+	int N = 1 << 15; // 15
 	std::cout << "N = " << N << std::endl;
-
+	
+	double (*T)(int) = T_invexp;
+	std::cout << "T:   T_invexp " << std::endl;
+	
 	// allocate buffers:
 	std::cout << "GENERATE BUFFERS" << std::endl;
 	double* beta_optimal = (double*) std::malloc(sizeof(double)*PARAMETER_P);
@@ -184,12 +293,69 @@ int main(int argc, char *argv[]){
 	// do experiments:
 	std::cout << "DO EXPERIMENTS" << std::endl;
 	
-	for(int i=1; i<6; i++)
+	double *b_curr = (double*) std::malloc(sizeof(double)*PARAMETER_P);
+        double *b_next = (double*) std::malloc(sizeof(double)*PARAMETER_P);
+        // rnd for first guess
+	std::random_device rd;
+        std::mt19937 gen_init(rd());
+        std::uniform_real_distribution<> dis_init(-10,10);
+	
+
+	// rnd for updates, delta_beta and u
+	std::default_random_engine generator;
+	std::normal_distribution<double> gaussian(0.0,LAMBDA);
+
+	std::random_device rd2;
+	std::mt19937 gen(rd2());
+	std::uniform_real_distribution<> uniform(0.0, 1.0);
+	
+
+	//for(int i=-10; i<6; i++)
 	{
-		double epsilon = std::pow(10, -i);
-		std::cout << "EPSILON = " << epsilon << std::endl;
+		double epsilon = std::pow(10, 10);
+		double epsilon_end = std::pow(10, -8);
 		
-		// TODO
+		// generate first guess:
+		for(int i=0; i<PARAMETER_P; i++)
+        	{
+                	b_curr[i] = dis_init(gen_init); // uniform
+        	}
+		
+		int k = 0;
+		double distance = epsilon*10;
+		do{
+			// do a step:
+			double error_now = 0;
+
+	                metropolis_step(
+	                        b_curr, X, N, y, T(k), b_next, error_now,
+	                        generator, gaussian,
+	                        std::sqrt(distance) * 0.1,
+				gen, uniform
+	                );
+	
+	                std::swap(b_curr, b_next);
+
+			// calculate distance to opt_beta:
+			distance = 0;
+			for(int i=0; i<PARAMETER_P; i++)
+			{
+				double e = beta_optimal[i] - b_curr[i];
+				distance+=e*e;
+				//std::cout << e << std::endl;
+			}
+			// check:
+			
+			//if(k%1000 == 0) std::cout << "k: " << k << ", dist: " << distance << std::endl;
+			
+			if(distance < epsilon)
+			{
+				std::cout << "REACHED " << epsilon << ", k: " << k << std::endl;
+				epsilon = epsilon/10.0;
+			}
+			k++;
+		}while(distance >= epsilon_end);
+		std::cout << "TERMINATE: " << k << std::endl;
 	}
 
 	// free buffers:
@@ -197,6 +363,9 @@ int main(int argc, char *argv[]){
 	std::free(beta_optimal);
 	std::free(X);
 	std::free(y);
+	
+	std::free(b_curr);
+        std::free(b_next);
 }
 
 void generate_data(double *b_opt, double *X, double *y, int N)
@@ -218,7 +387,7 @@ void generate_data(double *b_opt, double *X, double *y, int N)
 	}	
 	// create random data:
 	std::default_random_engine generator;
-	std::normal_distribution<double> gaussian(0.0,1.0);
+	std::normal_distribution<double> gaussian(0.0,0.1);
 
 	for(int line = 0; line < N; line++)
 	{
@@ -247,7 +416,7 @@ void generate_data(double *b_opt, double *X, double *y, int N)
 
 	for(int i=0; i<PARAMETER_P; i++)
 	{
-		b_opt[i] = y[i];
+		b_opt[i] = y_copy[i];
 	}
 	
 	std::free(beta);
